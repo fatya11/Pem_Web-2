6b0c49b71a76e82808867bebbd38af41bd6aebf9
fatya11 <114567379+fatya11@users.noreply.github.com>
2023-02-22 21:31:41 +0700

    Add files via upload

---
+++ pertemuan01/Praktik/variabel_user.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Fatya Khonsa";
+$prodi = "Sistem Informasi";
+$ipk = 4.00;
+
+echo "Nama saya adalah" . $nma . "<br>";
+echo "Program studi $prodi <br>";
+echo "IPK saya" . $ipk . ", aamiin";
