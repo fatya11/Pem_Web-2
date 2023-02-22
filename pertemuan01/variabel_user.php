e750364a4cc5287d5cac773cae577caaa02185fb
fatya11 <114567379+fatya11@users.noreply.github.com>
2023-02-22 21:29:39 +0700

    Delete pertemuan01 directory

--- pertemuan01/variabel_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nama = "Fatya Khonsa";
-$prodi = "Sistem Informasi";
-$ipk = 4.00;
-
-echo "Nama saya adalah" . $nma . "<br>";
-echo "Program studi $prodi <br>";
-echo "IPK saya" . $ipk . ", aamiin";
